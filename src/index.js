2ce796cf76e11ebccbe00ea2a8637a1cd4944fd3
Jounathan7 <jounathan@stu.usc.edu.tt>
2025-04-20 15:26:26 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -263,6 +263,7 @@ function renderAuthButton(user) {
 
   if (user) {
     authButtonContainer.innerHTML = `
+    <div class="flex-1">
       <button class="p-2 rounded-full text-gray-500 hover:text-gray-600">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -271,7 +272,8 @@ function renderAuthButton(user) {
       <a  href="dashboard.html" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">Welcome, ${user.email}</a>
       <button class="logout px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition-colors">
         Logout
-      </button>`;
+      </button>
+      </div>`;
   } else {
     authButtonContainer.innerHTML = `
       <a href="loginPage.html" class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors">
