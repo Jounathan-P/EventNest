214fe3b56f6c504c72638de45f0fef8e527b504e
Jounathan7 <jounathan@stu.usc.edu.tt>
2025-04-23 02:10:15 -0400

    hosting updates

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: '/src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'EventNest/dist'),
         filename: 'bundle.js'
     },
     watch: true
